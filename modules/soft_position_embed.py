cdb9c82e4c30ed5e63ee57a9709be7f0d968b7f8
Remi <omakinwa@tudelft.nl>
2026-04-01 01:34:28 +0200

    add: soft position embedding class and license notices

---
+++ modules/soft_position_embed.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+# Copyright 2026 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+Modification notice:
+- Extracted "build grid" and "SoftPositionEmbed" from the original "slot-attention.py" to this file
+'''
+
+import numpy as np
+import keras.layers as layers
+
+def build_grid(resolution):
+  ranges = [np.linspace(0., 1., num=res) for res in resolution]
+  grid = np.meshgrid(*ranges, sparse=False, indexing="ij")
+  grid = np.stack(grid, axis=-1)
+  grid = np.reshape(grid, [resolution[0], resolution[1], -1])
+  grid = np.expand_dims(grid, axis=0)
+  grid = grid.astype(np.float32)
+  return np.concatenate([grid, 1.0 - grid], axis=-1)
+
+
+class SoftPositionEmbed(layers.Layer):
+  """Adds soft positional embedding with learnable projection."""
+
+  def __init__(self, hidden_size, resolution):
+    """Builds the soft position embedding layer.
+
+    Args:
+      hidden_size: Size of input feature dimension.
+      resolution: Tuple of integers specifying width and height of grid.
+    """
+    super().__init__()
+    self.dense = layers.Dense(hidden_size, use_bias=True)
+    self.grid = build_grid(resolution)
+
+  def call(self, inputs):
+    return inputs + self.dense(self.grid)
